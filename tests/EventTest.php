f992dad0363d61251f1ac21fff44e9cc6d902494
mschielmann <michalschielmann@gmail.com>
2024-09-10 14:32:50 +0200

    Fix events test (#13)
    Make event tests more resilient
    
    ---------
    
    Co-authored-by: Michal Schielmann <michal.schielmann@shift4.com>

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -13,10 +13,13 @@ class EventTest extends AbstractGatewayTestBase
         $chargeRequest = Data::chargeRequest();
         $charge = $this->gateway->createCharge($chargeRequest);
 
-        $eventId = $this->gateway->listEvents()->getList()[0]->getId();
+        $events = $this->gateway->listEvents();
+        $expectedEvent = array_filter($events->getList(), function ($event) use ($charge) {
+            return $event->getData()->getId() === $charge->getId() && $event->getType() === 'CHARGE_SUCCEEDED';
+        })[0];
 
         // when
-        $event = $this->gateway->retrieveEvent($eventId);
+        $event = $this->gateway->retrieveEvent($expectedEvent->getId());
 
         // then
         Assert::assertChargeSucceededEvent($chargeRequest, $event);
@@ -34,20 +37,16 @@ class EventTest extends AbstractGatewayTestBase
 
         $listRequest = (new EventListRequest())
             ->includeTotalCount(true)
-            ->created((new CreatedFilter())->gte($charge1->getCreated()));
+            ->limit(100);
 
         // when
         $list = $this->gateway->listEvents($listRequest);
 
         // then
-        self::assertTrue($list->getTotalCount() >= 3);
-        $eventsForCreatedCharges = array_filter($list->getList(), function($charge) use ($expectedChargeIds)
-        {
-            return in_array($charge->getData()->getId(), $expectedChargeIds);
+        self::assertGreaterThanOrEqual(3, $list->getTotalCount());
+        $eventsForCreatedCharges = array_filter($list->getList(), function ($event) use ($expectedChargeIds) {
+            return in_array($event->getData()->getId(), $expectedChargeIds) && $event->getType() === 'CHARGE_SUCCEEDED';
         });
-        self::assertTrue(sizeOf($eventsForCreatedCharges) == 3);
-        foreach ($eventsForCreatedCharges as $event) {
-            Assert::assertEquals('CHARGE_SUCCEEDED', $event->getType());
-        }
+        self::assertEquals(3, sizeOf($eventsForCreatedCharges));
     }
 }
